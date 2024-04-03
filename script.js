0c865ed773ac149510aa4928d743d84b319222a3
synthpyt <synthpyt@gmail.com>
2024-04-04 01:26:51 +0200

    hello

--- script.js
+++ script.js
@@ -1,41 +1,30 @@
-$('h1').click(function(){
-    window.location.href='index.html';
-})
-
-
-// .ready() method makes it so as soon as the DOM is safe to manipulate the this function runs
 $(document).ready(function() {
-    $('button').click(function(){
-        let longUrl = $('input').val(); // Getting the value has to be called to actually get the value
-        shortenUrl(longUrl); // call the shorten url method on the LongUrl
-    });
+  $('button').click(function(){
+      let longUrl = $('input').val(); // Getting the value has to be called to actually get the value
+      shortenUrl(longUrl); // call the shorten url method on the LongUrl
+  });
 });
 
 function shortenUrl(longUrl) {
-    $.ajax({
-        url: 'https://urlbae.com/api/url/add',
-        method: "POST",
-        dataType: "json",
-        headers: {
-          'Authorization': 'Bearer 98baf6fab031f60734e639cf50134100',
-          'Content-Type': 'application/json',
-          'Access-Control-Allow-Origin': '*',
-          'Access-Control-Allow-Credintials': true
-        },
-        body: JSON.stringify({
-          "url": longUrl,
-        }),
-        success: function(response) {
-          if (response.success === 1) {
-            alert("Shortened URL:", response.data.url);
-          } else {
-            console.error("Error:", response.error.msg);
-          }
-        },
-        error: function(xhr, status, error) {
-          console.error("Error occurred while fetching the shortened URL:", error);
-        }
-      });
-    
-    
+  const data = {
+      url: longUrl,
+  };
+  axios.post('https://spoo.me/', data, {
+      headers: {
+          'content-type': 'application/x-www-form-urlencoded',
+          Accept: 'application/json',
+      },
+  })
+  .then(function (response) {
+      const shortUrl = response.data.short_url;
+      $('.shortened-container p').html("✅ Here's Your Short Link! <a href='" + shortUrl + "'>" + shortUrl + "</a> <button class='button' data-clipboard-text='" + shortUrl + "' onclick='showAlert()'><b>Copy</b></button>");
+      new ClipboardJS('.shortened-container button');
+  })
+  .catch(function (error) {
+      console.error(error);
+  });
+}
+
+function showAlert() {
+  $('.alert').show();
 }
